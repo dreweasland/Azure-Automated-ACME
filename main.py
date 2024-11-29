07ae957ce6f71ed971d07b17bbdd20ccbf0274fa
the-gabe <105918853+the-gabe@users.noreply.github.com>
2024-11-29 23:01:34 +0000

    Change User Agent

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def get_crt(account_key, csr, acme_dir, azure_keyvault_uri, log=LOGGER, disable_
     # helper function - make request and automatically parse json response
     def _do_request(url, data=None, err_msg="Error", depth=0):
         try:
-            resp = urlopen(Request(url, data=data, headers={"Content-Type": "application/jose+json", "User-Agent": "acme-tiny"}))
+            resp = urlopen(Request(url, data=data, headers={"Content-Type": "application/jose+json", "User-Agent": "Better-AGW-ACME"}))
             resp_data, code, headers = resp.read().decode("utf8"), resp.getcode(), resp.headers
         except IOError as e:
             resp_data = e.read().decode("utf8") if hasattr(e, "read") else str(e)
